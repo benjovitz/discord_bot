2a4bb5980b25f2319dd77f92aee745e24f98456f
Benjovitz <dbenjovitz@gmail.com>
2025-04-20 00:23:55 +0200

    trying

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers],
+});
+
+const commands = [
+    new SlashCommandBuilder().setName("tldr").setDescription("Summarize a message"),
+    new SlashCommandBuilder().setName("test").setDescription("Testing yo ass"),
+]
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === "test") {
+        await interaction.reply("GOOD MORNING VIETNAM");
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
