08c042ebe0f5c0bd98588245c8686aeef970c9a5
Benjovitz <dbenjovitz@gmail.com>
2025-04-20 00:34:08 +0200

    making commands work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Client, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
+import { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } from "discord.js";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
@@ -10,10 +10,25 @@ const client = new Client({
 const commands = [
     new SlashCommandBuilder().setName("tldr").setDescription("Summarize a message"),
     new SlashCommandBuilder().setName("test").setDescription("Testing yo ass"),
-]
+];
 
-client.once("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
+// Register slash commands
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+client.once("ready", async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+        console.log(`Logged in as ${client.user.tag}`);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 client.on("interactionCreate", async (interaction) => {
