b45a77a3f64f82c7e77b9b25d2cf54bd72fe6e1d
Benjovitz <dbenjovitz@gmail.com>
2025-04-20 00:28:42 +0200

    working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
 const commands = [
